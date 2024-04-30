5e22940c9d51a806bc894832dc66ca9f2cb982c9
Daniel-Albin <161868694+Daniel-Albin@users.noreply.github.com>
2024-04-30 15:04:50 -0400

    password gen excludes spaces and vertical tab in from the ascii table

--- PAC-Man/pythonFiles/passwordgen.py
+++ PAC-Man/pythonFiles/passwordgen.py
@@ -2,9 +2,8 @@ import secrets
 import string
 
 def password_gen(password_length):
-
-    characters = string.printable
-
-    secure_password = ''.join(secrets.choice(characters) for i in range(password_length))
-
-    return secure_password
+    characters = string.printable.replace(' ', '').replace('\x0b', '')
+    while True:
+        secure_password = ''.join(secrets.choice(characters) for I in range(password_length))
+        if len(secure_password) == password_length:
+            return secure_password
