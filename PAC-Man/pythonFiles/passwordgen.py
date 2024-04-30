3d572d7ccf6b00892e433e1f9b263b11f0b0dfe5
vharipal <91626199+vharipal@users.noreply.github.com>
2024-04-30 15:43:40 -0400

    Add files via upload

--- PAC-Man/pythonFiles/passwordgen.py
+++ PAC-Man/pythonFiles/passwordgen.py
@@ -2,7 +2,8 @@ import secrets
 import string
 
 def password_gen(password_length):
-    characters = string.printable.replace(' ', '').replace('\x0b', '')
+    #characters = string.printable.replace(' ', '').replace('\x0b', '').replace('    ','')
+    characters = string.ascii_letters + string.digits + string.punctuation.replace(' ','').replace('   ','').replace('|','').replace('(','').replace(')','').replace('{','').replace('}','').replace('[','').replace(']','')
     while True:
         secure_password = ''.join(secrets.choice(characters) for I in range(password_length))
         if len(secure_password) == password_length:
